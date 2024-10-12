d52cb7fc3a074c2362710c15af85a2bcb8b0ee8b
Yanis <ourir.yanis@gmail.com>
2024-10-12 21:26:49 +0200

    need improvements and other algorithms

--- app/Services/GameRecommendation.php
+++ app/Services/GameRecommendation.php
@@ -19,45 +19,29 @@ class GameRecommendation
     }
 
 
-    public function findEvaluationsOfUsersWhoRatedSameGames(array $users): array
+    public function filterEvaluationsOfUser(array $evaluations, string $userId): array
     {
-        $evaluations = [];
-        foreach($users as $user) {
-            $evaluations[] = collect($this->evaluationRepository->findEvaluationsByUserId($user))->filter(function ($evaluation) {
-                return $evaluation['rating'] > 7;
-            })->values();
-        }
-        return $evaluations;
-    }
 
-    public function excludeGamesAlreadyEvaluatedByUser(array $evaluations, string $userId): array
-    {
         $evaluations = collect($evaluations)->reject(function ($evaluation) use ($userId) {
             return $evaluation['user_id'] == $userId;
         })->values();
 
 
+        $evaluations = collect($evaluations)->reject(function ($evaluation) {
+            return $evaluation['rating'] < 7;
+        })->values();
+
         return $evaluations->toArray();
     }
 
-    public function filterGamesByEvaluation(array $evaluations): array
-    {
-        $games = [];
-        foreach($evaluations as $evaluation) {
-            $games[] = $this->gameRepository->findById($evaluation['game_id']);
-        }
-        return $games;
-    }
 
     public function findGamesThatUserCanLike(string $userId): array
     {
        $games = $this->gameRepository->findGamesOfCurrentUserLastEvaluations($userId); // RECOIS LES 10 DERNIERS JEUX QU'IL A NOTE > 7
        $evaluations = $this->evaluationRepository->findEvaluationsByGameIds($games); // RECUPERE LES EVALUATIONS DE CES JEUX
-       dd($evaluations);
-       // RECUPERER LES UTILISATEURS QUI ONT NOTE LES MEMES JEUX AVEC DES NOTES SIMILAIRES > 7
-        //RECUPERER LEURS AUTRES BONNES EVALUATIONS
-        // ENVOYER CES JEUX A NOTRE UTILISATEUR
-       $evaluationsFiltered = $this->excludeGamesAlreadyEvaluatedByUser($evaluations, $userId);
-       return $this->filterGamesByEvaluation($evaluationsFiltered);
+       $filteredEvaluations = $this->filterEvaluationsOfUser($evaluations, $userId); // FILTRE LES EVALUATIONS DE L'UTILISATEUR (GARDE CEUX DES AUTRES UTILISATEURS > 7)
+
+       // RECUPERER LES JEUX EVALUES PAR LES UTILISATEURS QUI ONT NOTE LES MEMES JEUX AVEC DES NOTES SIMILAIRES > 7
+       return $this->gameRepository->findGamesOfCurrentUserLastEvaluations($filteredEvaluations[0]['user_id']);
     }
 }
